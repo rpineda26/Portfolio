f10aee83b66100148ae52462fa521ad2a46f6904
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2024-04-13 22:11:19 +0800

    rename repo

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/portfolio';
+const base = '/career_portfolio';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
