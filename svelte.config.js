f374def6a815c67edba09b53dbdf417278783364
Zense <91407317+rpineda26@users.noreply.github.com>
2024-04-13 20:53:13 +0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/ralphpineda';
+const base = '/Portfolio';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
