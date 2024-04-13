6803da7ef3598c464262bcb42e498c5043b3fa5f
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2024-04-13 13:48:49 +0000

    Deploying to gh-pages from @ rpineda26/portfolio@71eae9d9bd22212f0afce138456532ddc8fb3cf7 🚀

---
+++ _app/immutable/chunks/stores.243eb256.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.53f71643.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
